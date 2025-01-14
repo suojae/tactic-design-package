7fca79174580846b59a30e5c115dafe19ac5239a
suojae <suojae.developer@gmail.com>
2025-01-14 20:27:50 +0900

    feat: tactic-textfield-size 추가 (#6)

---
+++ lib/components/text_fields/text_field_size.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/cupertino.dart';
+
+enum TacticTextFieldSize {
+  large,
+  medium,
+  small,
+}
+
+extension TactticTextFieldSizeExtension on TacticTextFieldSize {
+  double get height {
+    switch (this) {
+      case TacticTextFieldSize.large:
+        return 56.0;
+      case TacticTextFieldSize.medium:
+        return 48.0;
+      case TacticTextFieldSize.small:
+        return 40.0;
+    }
+  }
+
+  double get fontSize {
+    switch (this) {
+      case TacticTextFieldSize.large:
+        return 16.0;
+      case TacticTextFieldSize.medium:
+        return 14.0;
+      case TacticTextFieldSize.small:
+        return 12.0;
+    }
+  }
+
+  EdgeInsets get contentPadding {
+    switch (this) {
+      case TacticTextFieldSize.large:
+        return const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0);
+      case TacticTextFieldSize.medium:
+        return const EdgeInsets.symmetric(horizontal: 14.0, vertical: 12.0);
+      case TacticTextFieldSize.small:
+        return const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0);
+    }
+  }
+}
