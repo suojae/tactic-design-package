93c150258689f8f966422543dacad889c0afed28
suojae <suojae.developer@gmail.com>
2025-01-14 20:28:15 +0900

    feat: tactic-textfield-theme 추가 (#6)

---
+++ lib/components/text_fields/tactic_text_field_theme.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../../theme/colors.dart';
+
+final class TacticTextFieldTheme {
+  final Color backgroundColor;
+  final Color focusedBorderColor;
+  final Color errorBorderColor;
+  final Color disabledBackgroundColor;
+  final Color textColor;
+  final Color labelColor;
+  final Color iconColor;
+  final Color cursorColor;
+  final Color hintColor;
+
+  const TacticTextFieldTheme({
+    required this.backgroundColor,
+    required this.focusedBorderColor,
+    required this.errorBorderColor,
+    required this.disabledBackgroundColor,
+    required this.textColor,
+    required this.labelColor,
+    required this.iconColor,
+    required this.cursorColor,
+    required this.hintColor,
+  });
+
+  factory TacticTextFieldTheme.dark() {
+    return TacticTextFieldTheme(
+      backgroundColor: AppColors.base.deep,
+      focusedBorderColor: AppColors.primary.sky,
+      errorBorderColor: AppColors.semantic.red,
+      disabledBackgroundColor: AppColors.base.half,
+      textColor: AppColors.base.white,
+      labelColor: AppColors.base.gray,
+      iconColor: AppColors.base.gray,
+      cursorColor: AppColors.base.white,
+      hintColor: AppColors.base.gray,
+    );
+  }
+
+  factory TacticTextFieldTheme.light() {
+    return TacticTextFieldTheme(
+      backgroundColor: AppColors.base.white,
+      focusedBorderColor: AppColors.primary.blue,
+      errorBorderColor: AppColors.semantic.red,
+      disabledBackgroundColor: AppColors.base.second,
+      textColor: AppColors.base.black,
+      labelColor: AppColors.base.gray,
+      iconColor: AppColors.base.gray,
+      cursorColor: AppColors.base.black,
+      hintColor: AppColors.base.gray,
+    );
+  }
+}
